ed178e64661de68182b00f5ce65f4c9719d99e8e
Ivan Shutov <vanasutov793@gmail.com>
2024-08-07 22:06:29 +0400

    feat: add business=card component

--- src/shared/ui/button/ui/button.component.ts
+++ src/shared/ui/button/ui/button.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'app-button',
     standalone: true,
-    imports: [],
     templateUrl: './button.component.html',
     styleUrl: './button.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
