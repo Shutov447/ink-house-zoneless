67fd33cd6f07b5d9c37ae87298fdc26e5709db27
Ivan Shutov <vanasutov793@gmail.com>
2024-08-08 06:44:28 +0400

    refactor(scss style): create mixin instead

--- src/entities/reproductions/ui/reproduction-card.component.ts
+++ src/entities/reproductions/ui/reproduction-card.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ButtonComponent } from '@shared/ui';
+import { NgClass } from '@angular/common';
 import { IReproduction } from '../lib';
 
 @Component({
     selector: 'app-reproduction-card',
     standalone: true,
-    imports: [ButtonComponent],
+    imports: [ButtonComponent, NgClass],
     templateUrl: './reproduction-card.component.html',
     styleUrl: './reproduction-card.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
